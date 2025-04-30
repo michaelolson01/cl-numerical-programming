8a27d9a892202a090fca6e268a371dd0ab7ce3a4
michaelolson01 <michaelolson01@gmail.com>
2025-04-29 21:10:05 -0500

    Added some activation functions. Add more.

--- src/machine-learning/neural-network.lisp
+++ src/machine-learning/neural-network.lisp
@@ -19,15 +19,86 @@
                                                         (lambda () 0.0))
                                     accumulator))))
 
+;; helper functions
+;; only because it is faster to do complex calculations once, and then multiply the answers together.
+(defun ^2 (x)
+  (* x x))
+
+(defun ^3 (x)
+  (* x x x))
+
 ;; activation functions
+(defun unit-step (x)
+  (cond ((< x 0) 0)
+        ((= x 0) 0.5)
+        (t 1))) ;; (> x 0)
+
+(defun signum (x)
+  (cond ((< x 0) -1)
+        ((= x 0) 0)
+        (t 1))) ;; (> x 0)
+
+(defun linear (x)
+  x)
+
+(defun piece-wise-linear (x)
+  (cond ((>= x 0.5) 1)
+        ((or (< x -0.5) (> x 0.5)) (+ x 0.5))
+        (t 0))) ;; (<= x 0.5)
+
 (defun sigmoid (x)
   (/ 1 (+ 1 (exp (- x)))))
+
+(defun sigmoid-prime (x)
+  (* (sigmoid x) (- 1 (sigmoid x))))
+
+(defun shifted-relu (x &optional (shift -1))
+  (max shift x))
 
 (defun relu (x)
   (max 0 x))
 
+(defun relu-prime (x)
+  (if (> x 0)
+      1
+      0))
+
+(defun leaky-relu (x &optional (α 0.01))
+  (if (>= x 0)
+      x
+      (* α x)))
+
+(defun leaky-relu-prime (x &optional (α 0.01))
+  (if (> x 0)
+      1
+      α))
+
 (defun softplus (x)
   (log (+ 1 (exp x))))
+
+(defun softplus-prime (x)
+  (/ 1 (+ 1 (exp (- x)))))
+
+(defun elu (x &optional (α 1.0))
+  (if (> x 0)
+      x
+      (* α (- (exp x) 1))))
+
+(defun elu-prime (x &optional (α 1.0))
+  (if (> x 0)
+      1
+      (* α (exp x))))
+
+(defun selu (x &optional (α 1.0) (scale 1.0))
+  (if (> x 0)
+      (* s x)
+      (* s α (- (exp x) 1))))
+
+(defun selu-prime (x &optional (α 1.0) (scale 1.0))
+  (if (> x 0)
+      s
+      (* scale α (exp x))))
+
 
 ;; from wolfram
 ;; found on cppreference.com
@@ -44,6 +115,9 @@
 (defun tanh-3 (x)
   "This is taken from the Geeks for Geeks website."
   (- (/ 2 (+ 1 (exp (* -2 x)))) 1))
+
+(defun tanh-prime (x)
+  (- 1 (^2 (tanh x))))
 
 (defun apply-activation-function (function matrix)
   "Applies the activation function to every element of a matrix."
@@ -67,7 +141,7 @@
                              (cdr weights)
                              (cons (list (list inputs (first weights)) linear-hypothesis) caches)))))
 
-;; Neural network
+;; Neural network from Tariq Rashid' Book.
 (let ((input-nodes 3)
       (hidden-nodes 3)
       (output-nodes 3)
