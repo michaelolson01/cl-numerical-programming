140a02c234bdba049f61ea614854de7a31f14f34
Michael Olson <michaelolson01@gmail.com>
2025-05-20 09:25:33 -0500

    added matrix-reshape test 1

--- tests/main.lisp
+++ tests/main.lisp
@@ -7,5 +7,10 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-numerical-programming)' in your Lisp.
 
 (deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+    (testing "should (= 1 1) to be true"
+	   (ok (= 1 1))))
+
+(deftest matrix-reshape
+    (testing "matrix reshape test 1"
+	     (let ((matrix '((1 2 3) (4 5 6))))
+	       (ok (= (matrix-reshape matrix 2 3) '((1 2) (3 4) (5 6)))))))
